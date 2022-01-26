66218aaeaa18d6d068d0f737fc5f8375145d77d4
ElyBeske <ely.beske@yahoo.com>
2022-01-26 18:48:45 -0500

    second element

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 let header = document.querySelector('#page-header')
+let column = document.querySelectorAll('.column')
 header.style.backgroundColor = 'Blue'
+header.style.textAlign = 'right'
+column.style.backgroundColor = 'orange'
+column.style.textAlign = 'left'
+
+
