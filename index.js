39765b8ec7affe8107d4874a39f11eabb16e9d4d
ElyBeske <ely.beske@yahoo.com>
2022-01-26 18:57:16 -0500

    header font size

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ let header = document.querySelector('#page-header')
 let column = document.querySelectorAll('.column')
 header.style.backgroundColor = 'Blue'
 header.style.textAlign = 'right'
+header.style.fontSize = '100'
 for(let i = 0; i < column.length; i++){
-    column.style.backgroundColor = 'orange'
-    column.style.textAlign = 'left'
+    column[i].style.backgroundColor = 'orange'
+    column[i].style.textAlign = 'left'
 }
 
